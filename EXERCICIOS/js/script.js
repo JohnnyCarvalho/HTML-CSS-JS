9364b394d08fb00f23046cf6fd7bf8cb659ac084
johnnyCarvalhoDev <johnnycarvalhodev@gmail.com>
2022-03-02 08:38:02 -0300

    EX 02/03

---
+++ EXERCICIOS/js/script.js
@@ -0,0 +1,19 @@
+console.log('Hello Word')
+
+const numero = 1
+var numero2 = 2
+
+console.log(numero)
+console.log(numero2)
+
+var numero1 = 3
+
+console.log(numero1)
+
+if (numero%2==0) {
+    console.log('Par')
+    
+}
+else{
+    console.log('Impar')
+}
